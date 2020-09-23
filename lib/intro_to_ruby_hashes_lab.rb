b4373e406fa30e9aee2dc747fccc4264a7c1ddb4
Eric Uberman <euberman@gmail.com>
2020-09-23 16:54:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,6 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  result = { #{key} => #{value} }
-  result
+  { #{key} => #{value} }
 end
 
 def read_from_hash(hash, key)
