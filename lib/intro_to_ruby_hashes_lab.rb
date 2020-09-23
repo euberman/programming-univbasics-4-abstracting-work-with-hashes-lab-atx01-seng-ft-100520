7795bceffc70868e5e85de85cf326ab2652a99bd
Eric Uberman <euberman@gmail.com>
2020-09-23 17:08:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,7 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  hash[key]
+  puts hash[key]
 end
 
 def update_counting_hash(hash, key)
@@ -18,4 +18,5 @@ def update_counting_hash(hash, key)
   else
     hash[key] = 1 
   end
+  puts hash
 end
